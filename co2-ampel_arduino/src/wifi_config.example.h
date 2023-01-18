3d0d210b56d1df0f947472b75b2208d95f8dffb2
Matthias Büchner <m.buechner@gmail.com>
2023-01-19 00:08:02 +0100

    update wifi example config

--- co2-ampel_arduino/src/wifi_config.example.h
+++ co2-ampel_arduino/src/wifi_config.example.h
@@ -2,8 +2,19 @@
 #define WIFI_CONFIG_H
 
 // the ssid of the WiFi
-#define WIFI_CONFIG_SSID        "wifi-ap-ssid"
+#define WIFI_CONFIG_SSID_1        "WiFi 1"
 // the password of the WiFi (comment out if open)
-#define WIFI_CONFIG_PASSWORD    "super-secure-password"
+#define WIFI_CONFIG_PASSWORD_1    "password_1"
+
+// the ssid of the WiFi
+#define WIFI_CONFIG_SSID_2        "WiFi 2"
+// the password of the WiFi (comment out if open)
+#define WIFI_CONFIG_PASSWORD_2    "password_3"
+
+// the ssid of the WiFi
+#define WIFI_CONFIG_SSID_3        "WiFi 3"
+// the password of the WiFi (comment out if open)
+#define WIFI_CONFIG_PASSWORD_3    "password_3"
+
 
 #endif //WIFI_CONFIG_H
